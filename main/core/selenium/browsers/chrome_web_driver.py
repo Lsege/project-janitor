1f6cbaef9a5cad25c4628f3a02f817a3b6b8d4a6
Lsege <sanabrialucas97@gmail.com>
2023-01-03 22:47:20 -0300

    chrome web driver added

---
+++ main/core/selenium/browsers/chrome_web_driver.py
@@ -0,0 +1,10 @@
+"""Module to define Chrome Web Driver
+"""
+from selenium import webdriver
+
+
+class ChromeWebDriver:
+    """Chrome Web Driver implementation
+    """
+    type_browser = None
+    
